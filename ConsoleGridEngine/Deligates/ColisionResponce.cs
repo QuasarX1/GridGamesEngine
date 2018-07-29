51d9ee7bdef7e9e45e40f484a6591780d31803cd
QuasarX1 - BuildTools <chris.rowe19@outlook.com>
2018-07-30 00:18:39 +0100

    Updated repo to only include GridGamesEngine project

--- ConsoleGridEngine/Deligates/ColisionResponce.cs
+++
@@ -1,6 +0,0 @@
-﻿using ConsoleGridEngine.Entities;
-
-namespace ConsoleGridEngine.Deligates
-{
-    public delegate void ColisionResponce(IEntity entity, string[] stringParams);
-}
--- ConsoleGridEngine/Deligates/Responce.cs
+++
@@ -1,6 +0,0 @@
-﻿using ConsoleGridEngine.Entities;
-
-namespace ConsoleGridEngine.Deligates
-{
-    public delegate bool? Responce(IPlayer entity = null);
-}
